fd069732b3f12e96e65b7d90a4d48c207357a6cb
un1x2 <ziyodilloyigitaliyev365@gmail.com>
2024-09-30 22:21:26 +0500

    update

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import *
 
+
 class ProductMediaInline(admin.TabularInline):
     model = ProductMedia
     extra = 1  # Add one extra empty form to add a new media file
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title_uz', 'description_uz', 'price')
+    list_display = ('id', 'title_uz', 'short_description', 'price')
     inlines = [ProductMediaInline]
+
+    def short_description(self, obj):
+        # Descriptionni 100 so'zga qisqartirish
+        return ' '.join(obj.description_uz.split()[:100]) + '...' if len(obj.description_uz.split()) > 100 else obj.description_uz
+
+    short_description.short_description = 'Description'  # Admin panelda ko'rinadigan nom
+
 
 @admin.register(ProductMedia)
 class ProductMediaAdmin(admin.ModelAdmin):
