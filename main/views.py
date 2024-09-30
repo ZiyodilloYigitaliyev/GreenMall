6466aafe999cc8e564dab7bb2f98f69d0268782f
un1x2 <ziyodilloyigitaliyev365@gmail.com>
2024-10-01 00:38:24 +0500

    update

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import TokenAuthentication
 from .models import *
 from .serializers import *
 
@@ -6,10 +8,37 @@ class StatsListView(generics.ListAPIView):
     queryset = Stats.objects.all()
     serializer_class = StatsSerializer
 
+
+# Custom permission class
+class AllowAnyGetAuthenticatedOther(IsAuthenticated):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return True
+        return super().has_permission(request, view)
+
+
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    # GET uchun ruxsat, POST uchun token talab qilinadi
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+
 class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    authentication_classes = [TokenAuthentication]
+
+    # GET uchun ruxsat, PUT/DELETE uchun token talab qilinadi
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
