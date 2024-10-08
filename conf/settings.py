bcd7974eea4d8e55d744900bce9b0f87e27a444e
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-10-08 21:47:11 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -129,7 +129,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitalo
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Fayl URL larini yaratish
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
