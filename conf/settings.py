0c250f2b2f2554318ea313d08977fda2e8d453d1
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-10-08 20:57:02 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -121,7 +121,7 @@ AWS_STORAGE_BUCKET_NAME = 'greenwall'  # Bu sizning space nomingiz
 
 # DigitalOcean Spaces regionini kiriting, masalan Frankfurt 'fra1'
 AWS_S3_REGION_NAME = 'fra1'
-AWS_S3_ENDPOINT_URL = 'fra1.digitaloceanspaces.com'  # DO Spaces uchun endpoint
+AWS_S3_ENDPOINT_URL = 'https://greenwall.fra1.digitaloceanspaces.com'  # DO Spaces uchun endpoint
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_REGION_NAME}.digitaloceanspaces.com'
 
@@ -130,7 +130,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Fayl URL larini yaratish
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Agar kerak bo'lsa, fayl caching opsiyalarini ham qo'shishingiz mumkin
@@ -141,7 +141,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Database
