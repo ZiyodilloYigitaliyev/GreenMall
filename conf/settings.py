83a6dd29aa4109561fcfa9a96a84357eec82b4c7
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-10-08 20:16:05 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 
 # Statik fayllar
 # STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
