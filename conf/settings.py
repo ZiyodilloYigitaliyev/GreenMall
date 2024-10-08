cf33187b3f9320c41721513217b1648f3c129020
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-10-08 22:57:51 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -127,8 +127,8 @@ AWS_S3_ENDPOINT_URL = 'https://greenwall.fra1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',  # Bir kunlik kechlash (caching) muddati
 }
-AWS_MEDIA_LOCATION = 'media'
-PUBLIC_MEDIA_LOCATION = 'media'
+AWS_MEDIA_LOCATION = 'media/'
+PUBLIC_MEDIA_LOCATION = 'media/'
 MEDIA_URL = '%s%s' % (AWS_S3_ENDPOINT_URL, AWS_MEDIA_LOCATION)
 DEFAULT_FILE_STORAGE = 'django_project.storage_backends.MediaStorage'
 
