80aa044818c9533463d8e6de5662f61a0ee1bb63
Cik232 <azamjon23202@gmail.com>
2024-10-08 13:18:13 +0500

    update

--- work/views.py
+++ work/views.py
@@ -2,12 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Project
 from .serializers import ProjectSerializer
 from django.shortcuts import get_object_or_404
 
 class ProjectListCreateView(APIView):
-
+    parser_classes = (MultiPartParser, FormParser)  # Fayl yuklashni qo'llab-quvvatlaydi
     def get_permissions(self):
         if self.request.method == 'GET':
             return [AllowAny()]  # GET so'rovlar uchun autentifikatsiya talab qilinmaydi
