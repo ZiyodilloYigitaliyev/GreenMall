8629c154d864b770e3c48b2cbb9ac7e04c673081
Cik232 <azamjon23202@gmail.com>
2024-10-09 15:04:07 +0500

    update

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import TokenAuthentication
 from .models import *
 from .serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 class StatsListView(generics.ListAPIView):
     queryset = Stats.objects.all()
@@ -35,15 +38,48 @@ class OrderListCreateView(generics.ListCreateAPIView):
         return super().get_permissions()
 
 
-class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+class OrderDetailView(APIView):
     authentication_classes = [TokenAuthentication]
 
-    # GET uchun ruxsat, PUT/DELETE uchun token talab qilinadi
     def get_permissions(self):
-        if self.request.method == 'POST':
+        if self.request.method == 'GET':
             self.permission_classes = [AllowAny]
         else:
             self.permission_classes = [IsAuthenticated]
         return super().get_permissions()
+
+    def get(self, request, pk):
+        try:
+            order = Order.objects.get(pk=pk)
+            serializer = OrderSerializer(order)
+            return Response(serializer.data)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        try:
+            order = Order.objects.get(pk=pk)
+            serializer = OrderSerializer(order, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request, pk):
+        try:
+            order = Order.objects.get(pk=pk)
+            order.is_verified = not order.is_verified  # Qiymatni o'zgartirish
+            order.save()
+            return Response({"message": "Order status updated"}, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            order = Order.objects.get(pk=pk)
+            order.delete()
+            return Response({"message": "Order deleted"}, status=status.HTTP_204_NO_CONTENT)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
