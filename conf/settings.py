b784b359bc111be8f78a084f2a2ad2d46cf0ff83
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-11-13 04:50:56 +0500

    *

--- conf/settings.py
+++ conf/settings.py
@@ -107,6 +107,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("BUCKETEER_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.getenv("BUCKETEER_AWS_REGION")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
 
 # Statik va media fayllar uchun sozlamalar
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
