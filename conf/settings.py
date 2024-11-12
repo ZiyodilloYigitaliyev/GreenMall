74acb07bcb49fbf9a48c8742857e473ffd4363fd
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-11-13 04:29:35 +0500

    update

--- conf/settings.py
+++ conf/settings.py
@@ -114,6 +114,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATABASES = {
     'default': {
