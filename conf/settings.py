c5e677b90062efb87eba1058d56c8b58e88be319
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-11-13 18:53:48 +0500

    *

--- conf/settings.py
+++ conf/settings.py
@@ -101,13 +101,15 @@ TEMPLATES = [
 ]
 
 
-# S3 Bucketeer uchun sozlamalar
+
+# BUCKETEER uchun sozlamalar
 AWS_ACCESS_KEY_ID = os.getenv("BUCKETEER_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("BUCKETEER_AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("BUCKETEER_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.getenv("BUCKETEER_AWS_REGION")
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 
 # Statik va media fayllar uchun sozlamalar
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
@@ -115,7 +117,16 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# S3 bucketning fayllari uchun public ruxsatni sozlash
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read',
+}
+
+
 
 DATABASES = {
     'default': {
