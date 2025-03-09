cf5868c2746a46e1a61e432962fbce5962b62a50
O'tkir <4dm1n217@gmail.com>
2025-03-10 03:02:53 +0500

    Update pdf_generator.py

--- warranty_card/pdf_generator.py
+++ warranty_card/pdf_generator.py
@@ -16,7 +16,7 @@ s3_client = boto3.client(
     "s3",
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_S3_REGION_NAME,
+    region_name=settings.AWS_REGION_NAME,
 )
 
 
