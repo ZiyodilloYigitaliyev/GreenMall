10f77d3331cd50e77cbd32239f9c800c56f1d408
O'tkir <4dm1n217@gmail.com>
2025-03-13 02:21:29 +0500

    Update pdf_generator.py

--- warranty_card/pdf_generator.py
+++ warranty_card/pdf_generator.py
@@ -17,9 +17,8 @@ s3_client = boto3.client(
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     region_name=settings.AWS_REGION_NAME,
-    ContentType="application/pdf",
-    ACL="public-read",
 )
+
 
 
 
