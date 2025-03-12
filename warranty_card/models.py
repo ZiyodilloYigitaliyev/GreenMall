57c00422e8aef8cc7cef6f59d55196011b494c23
O'tkir <4dm1n217@gmail.com>
2025-03-13 04:11:46 +0500

    ***

--- warranty_card/models.py
+++ warranty_card/models.py
@@ -3,13 +3,10 @@ from django.db import models
 import random
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, unique=True)
     address = models.TextField()
-    password = models.CharField(max_length=255)
     unique_code = models.IntegerField(unique=True, blank=True, null=True)
 
     def save(self, *args, **kwargs):
--- warranty_card/serializers.py
+++ warranty_card/serializers.py
@@ -5,9 +5,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["name", "surname", "email", "phone", "address", "password", "unique_code"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        validated_data["password"] = make_password(validated_data["password"])
-        return super().create(validated_data)
+        fields = "__all__"
