6bd6848b1a9029aaa241ac4cbb748d12c737db3a
O'tkir <4dm1n217@gmail.com>
2025-03-13 01:49:22 +0500

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -73,9 +73,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:5173",
-    "https://green-mall.vercel.app",
     
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
